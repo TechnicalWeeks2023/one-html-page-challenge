0bea959ed443d70a779b3db1ae7e58611d2fee51
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-11-15 18:09:36 +0200

    GIT ACTIONS

---
+++ run_and_display.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Assuming your Docker image is named "your-image-name"
+docker run mcr.microsoft.com/mssql/server:latest
+
+# Displaying your first and last name
+echo "TAISIIA KYSHYNSKA"
